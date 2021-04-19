d3c95cf8feca13c0fbb8dbc3ee0bf213ff1382db
keita86 <ke4i2ta4@gmail.com>
2021-04-19 06:21:53 +0000

    [item 作成途中]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
+
   root :to => "homes#top"
   get "homes/about" => "homes#about"
 end
