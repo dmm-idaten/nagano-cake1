55e89c4d14bd15324c40754edfc40e5c689d2120
keita86 <ke4i2ta4@gmail.com>
2021-04-25 05:27:02 +0000

    [売り切れ表示を追加しました]

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:create, :index, :edit, :update]
-    resources :orders, only: [:show, :update]
+    resources :orders, only: [:index, :show, :update]
     resource :order_details, only: [:update]
   end
 
