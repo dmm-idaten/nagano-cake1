b5bbeaf2beb1e19ab81ce5aec00a08932a805323
Maxi-chelsea <maxi19950049@gmail.com>
2021-04-27 00:51:36 +0000

    update_order

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ApplicationRecord
   belongs_to :customer
 
   def full_name
-    self.postal_code + self.address + self.name
+    "〒" + self.postal_code + "　" + self.address + "　" + self.name
   end
 
 end
