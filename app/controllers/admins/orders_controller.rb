5a33abf4e865f7b84d0477cb93327c66fe8b3dee
Maxi-chelsea <maxi19950049@gmail.com>
2021-04-27 06:50:50 +0000

    update

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,14 +1,24 @@
 class Admins::OrdersController < ApplicationController
-	before_action :authenticate_admin!
+  before_action :authenticate_admin!
 
-	def index
-		@orders = Order.all
-	end
+  def index
+	@orders = Order.all
+  end
 
-	def show
-	end
+  def show
+	@order = Order.find(params[:id])
+    @order_details = @order.order_details
+  end
 
-	def update
-	end
+  def update
+	order = Order.find(params[:id])
+	order.update(order_status: params[:order][:order_status].to_i)
+	redirect_to admins_order_path(order)
+  end
+
+  private
+  def order_params
+  	params.require(:order).permit(:order_status)
+  end
 
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,9 +1,17 @@
 module OrdersHelper
-	  def total_price(carts)
-    total = 0
-    carts.each do |cart|
-      total = cart.subtotal + total
-    end
-    return total
+  def total_price(carts)
+      total = 0
+      carts.each do |cart|
+        total = cart.subtotal + total
+      end
+      return total
+  end
+
+  def total_amount(orderorder_details)
+      total = 0
+      orderorder_details.each do |order_detail|
+        total = order_detail.amount + total
+      end
+      return total
   end
 end
